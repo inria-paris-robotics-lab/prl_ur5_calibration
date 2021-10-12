a43b5bc1d02548fcad3685449f2dcdb5bc160aa7
EtienneAr <earlaud@pl346-pro.paris.inria.fr-1>
2021-10-12 15:32:46 +0200

    First optim - is result good ?

--- optim.py
+++ optim.py
@@ -1,9 +1,10 @@
 from data_2 import data
 from scipy.optimize import fmin_bfgs
 import pinocchio as pin
+from math import cos, sqrt
 
-camera_transf = [0.0, 0.02, 0.15],  [-0.5, -0.5, 0.5, 0.5]
-arm_transf = [-0.18, 0.0, 0.79],   [0, -0.924, 0, 0.38]
+q_cam_0 = [0.0,   0.02, 0.15,  -0.5, -0.5,   0.5, 0.5]
+q_arm_0 = [-0.18, 0.0,  0.79,  0,    -0.924, 0,   0.38]
 
 # PARSE (xyz, quat) INTO se3
 def parseLine(line):
@@ -17,13 +18,41 @@ def parseLine(line):
 def transf_to_se3(transf):
     return pin.XYZQUATToSE3(list(transf[0]) + list(transf[1]))
 
+# COMPUTE
+def error(T):
+    v = pin.log(T).vector
+    return (v**2).sum()
+
+def normalize(quat):
+    norm_sq = 0
+    for c in quat:
+        norm_sq += c**2
+    norm = sqrt(norm_sq)
+    return [c/norm for c in quat]
+
+def x_to_qs(x):
+    q1 = list(x)[:7]
+    q2 = list(x)[7:]
+
+    q1 = q1[:3] + normalize(q1[3:])
+    q2 = q2[:3] + normalize(q2[3:])
+    
+    return q1, q2
+
+def diff(a, b):
+    assert len(a) == len(b), "Cannot diff different sizes"
+    return [a[i] - b[i] for i in range(len(a))]
+
+
 # DEBUG FUNCTIONS
 def print_all_subtransforms(transform_list, name_list):
+    T = None
     for j in range(len(transform_list)+1):
         T = transf_to_se3([[0,0,0], [0,0,0,1]])
         for i in range(j):
             T = T*transform_list[i]
         print_transform(T, name_list[j])
+    return T # return the last Transform
 
 def print_transform(T, name):
     XYZQuat = pin.SE3ToXYZQUAT(T)
@@ -33,12 +62,45 @@ def print_transform(T, name):
     print("Quat : " + str(XYZQuat[3:]))
     print("----")
 
-# TEST ON FIRST EXAMPLE
-base_T_stand, shoulder_T_tool, screws_T_cam, cam_T_aruco = parseLine(data[0])
-tool_T_screws = transf_to_se3(camera_transf)
-stand_T_shoulder = transf_to_se3(arm_transf)
+# # TEST ON FIRST EXAMPLE
+# base_T_stand, shoulder_T_tool, screws_T_cam, cam_T_aruco = parseLine(data[0])
+# tool_T_screws = transf_to_se3(camera_transf)
+# stand_T_shoulder = transf_to_se3(arm_transf)
 
-transform_list = [base_T_stand, stand_T_shoulder, shoulder_T_tool, tool_T_screws, screws_T_cam, cam_T_aruco]
-name_list = ["base", "stand", "shoulder", "tool", "screws", "cam", "aruco"]
+# transform_list = [base_T_stand, stand_T_shoulder, shoulder_T_tool, tool_T_screws, screws_T_cam, cam_T_aruco]
+# name_list = ["base", "stand", "shoulder", "tool", "screws", "cam", "aruco"]
 
-print_all_subtransforms(transform_list, name_list)
+# T = print_all_subtransforms(transform_list, name_list)
+# print(pin.log(T))
+# print(error(T))
+
+# OPTIMIZE
+def cost(x):
+    q_cam, q_arm = x_to_qs(x)
+
+    tool_T_screws = pin.XYZQUATToSE3(q_cam)
+    stand_T_shoulder = pin.XYZQUATToSE3(q_arm)
+
+    c = 0
+    for d in data:
+        base_T_stand, shoulder_T_tool, screws_T_cam, cam_T_aruco = parseLine(d) 
+        T = base_T_stand * stand_T_shoulder * shoulder_T_tool * tool_T_screws * screws_T_cam * cam_T_aruco
+        c += error(T)
+    return c
+
+x0 = q_cam_0 + q_arm_0
+
+xopt_bfgs = fmin_bfgs(cost, x0)
+
+# Normalize quaternions
+qopt_cam, qopt_arm = x_to_qs(xopt_bfgs)
+
+print("Camera : ")
+print(f"qopt_cam: {qopt_cam}\ndiff : {diff(qopt_cam, q_cam_0)}")
+print("Arm : ")
+print(f"qopt_arm: {qopt_arm}\ndiff : {diff(qopt_arm, q_arm_0)}")
+
+print(f"Start cost : {cost(x0)}")
+print(f"Final cost : {cost(xopt_bfgs)}")
+
+
