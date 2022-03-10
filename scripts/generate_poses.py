65375d77c974523415483f6647007803aa8c3b1e
EtienneAr <>
2022-03-10 15:54:20 +0100

    Better config file; allow for not centered marker tag

--- scripts/generate_poses.py
+++ scripts/generate_poses.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pickle
 from tqdm import tqdm
+import rospkg
+import yaml
 
 def sq_dist(p1, p2):
     ''' Squared distance between two points '''
@@ -55,46 +57,40 @@ def plot_points(points, ax = None, color = 'b', marker = '.', markersize=15):
 
     return ax
 
+def rpy_to_pose(center, l, rpy):
+    ''' Convert a spherical coordinates into a cartesian pose (with the camera optical frame pointing to the orgin) '''
+    # c = camera optical frame
+    # t = tool frame
+    # g = goal frame
+    # b = base frame
+    # w = world frame
+
+    tMc = pin.XYZQUATToSE3([0.032, 0.033, -0.081, 0, 0, 1, 0]) # TODO: Do not hardcode !
+
+    wMb = pin.XYZQUATToSE3(center["xyz"] + euler_to_quaternion(center["rpy"])) # Rotate the base frame
+    bMb = pin.XYZQUATToSE3([0, 0, 0] + euler_to_quaternion(rpy)) # Rotate the base frame
+    bMc = pin.XYZQUATToSE3([0,0,l, 0,1,0,0])
+
+    wMt = wMb * bMb * bMc * tMc.inverse()
+
+    # pose
+    xyzquat_look = pin.SE3ToXYZQUAT(wMt)
+    pose_look = [list(xyzquat_look[:3]), list(xyzquat_look[3:] / np.linalg.norm(xyzquat_look[3:]))]
+
+    return pose_look
+
 class PoseGenerator():
-    def __init__(self) -> None:
-        import rospkg
-        current_path = rospkg.RosPack().get_path("prl_ur5_calibration")
-        self.files_path = current_path + "/files/"
-
-    def rpy_to_pose(self, l, rpy):
-        ''' Convert a spherical coordinates into a cartesian pose (with the camera optical frame pointing to the orgin) '''
-        # c = camera optical frame
-        # t = tool frame
-        # g = goal frame
-        # b = base frame
-
-        tMc = pin.XYZQUATToSE3([0.032, 0.033, -0.081, 0, 0, 1, 0])
-
-        bMb = pin.XYZQUATToSE3([0, 0, 0] + euler_to_quaternion(rpy)) # Rotate the base frame
-        bMc = pin.XYZQUATToSE3([0,0,l, 0,1,0,0])
-
-        bMt = bMb * bMc * tMc.inverse()
-
-        # pose
-        xyzquat_look = pin.SE3ToXYZQUAT(bMt)
-        pose_look = [list(xyzquat_look[:3]), list(xyzquat_look[3:] / np.linalg.norm(xyzquat_look[3:]))]
-
-        return pose_look
-
+    def __init__(self, abs_path, cfg):
+        self.abs_path = abs_path
+        self.cfg = cfg
 
     def generate_samples(self):
         ''' Generate a set of tool poses such that the camera frame is on a sphere (centered on the origin) and pointing to the origin. '''
-        print("Generating samples...")
-
-        # Read configuration file
-        cfg_file = open(self.files_path + "sample_config.yaml", "r")
-        import yaml
-        cfg = yaml.safe_load(cfg_file)
-        cfg_file.close()
+        rospy.loginfo("Generating samples...")
 
         # Generate angles to try
-        sample_distance = cfg["distance"]
-        interest_angles = [eval(angle) if isinstance(angle, str) else angle for angle in cfg["angles"]]
+        sample_distance = self.cfg["generation"]["distance"]
+        interest_angles = [eval(angle) if isinstance(angle, str) else angle for angle in self.cfg["generation"]["angles"]]
         sample_rpy = []
         for r in tqdm(interest_angles):
             for p in interest_angles:
@@ -102,13 +98,13 @@ class PoseGenerator():
                     sample_rpy.append([r, p, y])
         sample_rpy = sample_rpy
 
-        sample_pose = [self.rpy_to_pose(sample_distance, rpy) for rpy in sample_rpy]
-        pickle.dump(sample_pose, open(self.files_path + "poses_sample.p", "wb"))
-        print(F"{len(sample_pose)} samples generated.\n")
+        sample_pose = [rpy_to_pose(self.cfg["marker_pose"], sample_distance, rpy) for rpy in sample_rpy]
+        pickle.dump(sample_pose, open(self.abs_path + self.cfg["output_paths"]["all"], "wb"))
+        rospy.loginfo(F"{len(sample_pose)} samples generated.\n")
 
     def filter_reachable(self):
         ''' Filter all the sampled poses. Keep the one that have valid solutions (without collision). '''
-        print("Filtering reachable poses...")
+        rospy.loginfo("Filtering reachable poses...")
         # Init the robot and planner
         from prl_hpp.ur5 import robot, planner
         planner.lock_grippers()
@@ -117,33 +113,35 @@ class PoseGenerator():
         planner.set_acceleration_limit(0.25)
 
         # Load the sampled poses
-        poses_sample = pickle.load(open(self.files_path + "poses_sample.p", "rb"))
+        poses_sample = pickle.load(open(self.abs_path + self.cfg["output_paths"]["all"], "rb"))
 
         def is_pose_achievable(pose):
             ''' Test if a pose is achievable by the robot (path and final pose)'''
             try:
                 planner.make_gripper_approach(robot.left_gripper_name, *pose, approach_distance = 0, do_not_plan = True)
             except AssertionError as e:
-                # print(e)
+                # rospy.logwarn(e)
                 return False
             return True
 
         # Filter all the non reachable poses
         poses_reachable = []
         for pose in tqdm(poses_sample):
+            assert not rospy.is_shutdown(), "ROS shutdown"
             if pose[0][2] < 0: # Basic filter as the camera shouldn't be below the table surface...
                 continue
             if is_pose_achievable(pose):
                 poses_reachable.append(pose)
 
-        pickle.dump(poses_reachable, open(self.files_path + "poses_reachable.p", "wb"))
-        print(F"{len(poses_reachable)} reachable poses found from samples.\n")
+        pickle.dump(poses_reachable, open(self.abs_path + self.cfg["output_paths"]["reachable"], "wb"))
+        rospy.loginfo(F"{len(poses_reachable)} reachable poses found from samples.\n")
 
-    def filter_decimate(self, n):
+    def filter_decimate(self):
         ''' Find the n "most representative" poses (ie. the one that are the most spaced out) from the all reachable poses. '''
-        print("Decimate reachable poses...")
+        rospy.loginfo("Decimate reachable poses...")
         # Load the reachable poses
-        poses = pickle.load(open(self.files_path + "poses_reachable.p", "rb"))
+        poses = pickle.load(open(self.abs_path + self.cfg["output_paths"]["reachable"], "rb"))
+        n = self.cfg["n_final_poses"]
 
         if(len(poses) > n):
             pbar = tqdm(total=len(poses)-n)
@@ -153,13 +151,16 @@ class PoseGenerator():
                 poses = remove_closest(poses, 1)
                 pbar.update(1)
 
-        pickle.dump(poses, open(self.files_path + "poses_calibration.p", "wb"))
-        print(F"Done decimating. {len(poses)} reachable poses kept.\n")
+        pickle.dump(poses, open(self.abs_path + self.cfg["output_paths"]["final"], "wb"))
+        rospy.loginfo(F"Done decimating. {len(poses)} reachable poses kept.\n")
 
 
 node = None
 if __name__ == "__main__":
-    rospy.init_node("generate_poses", anonymous=True)
+    rospy.init_node("generate_poses", anonymous=True, log_level=rospy.INFO)
+
+    # Read parameters
+    config_file = rospy.get_param("~config_file")
 
     generate_samples = rospy.get_param("~generate_samples", True) # Generate or not the initial sampling of space
     filter_reachable = rospy.get_param("~filter_reachable", True)   # Filter the initial sampling : test is each pose can be achieved by the robot
@@ -170,32 +171,41 @@ if __name__ == "__main__":
     plot_reachables = rospy.get_param("~plot_reachables", True)
     plot_finals = rospy.get_param("~plot_finals", True)
 
-    n_poses = rospy.get_param("~n_poses", 20) # Number of final poses to generate
+    abs_path = rospkg.RosPack().get_path("prl_ur5_calibration") + "/"
 
-    node = PoseGenerator()
+    # Read config file
+    cfg_file = open(abs_path + config_file, "r")
+    cfg = yaml.safe_load(cfg_file)
+    cfg_file.close()
 
-    if generate_samples:
+    # Instatiate main class
+    node = PoseGenerator(abs_path, cfg)
+
+    # Run generation steps
+    if generate_samples and not rospy.is_shutdown():
         node.generate_samples()
 
-    if filter_reachable:
+    if filter_reachable and not rospy.is_shutdown():
         node.filter_reachable()
 
-    if filter_decimate:
-        node.filter_decimate(n_poses)
+    if filter_decimate and not rospy.is_shutdown():
+        node.filter_decimate()
 
-    ax = None
-    legend = []
-    if create_plot:
+    # Plot
+    if create_plot and not rospy.is_shutdown():
+        ax = None
+        legend = []
+
         if plot_samples:
-            poses_sample = pickle.load(open(node.files_path + "poses_sample.p", "rb"))
+            poses_sample = pickle.load(open(abs_path + cfg["output_paths"]["all"], "rb"))
             ax = plot_points(poses_sample, ax=ax, color='k', marker='.', markersize=5)
             legend.append("Sampled poses")
         if plot_reachables:
-            poses_reachables = pickle.load(open(node.files_path + "poses_reachable.p", "rb"))
+            poses_reachables = pickle.load(open(abs_path + cfg["output_paths"]["reachable"], "rb"))
             ax = plot_points(poses_reachables, ax=ax, color='b', marker='2', markersize=25)
             legend.append("Reachables poses")
         if plot_finals:
-            poses_final = pickle.load(open(node.files_path + "poses_calibration.p", "rb"))
+            poses_final = pickle.load(open(abs_path + cfg["output_paths"]["final"], "rb"))
             ax = plot_points(poses_final, ax=ax, color='r', marker='v', markersize=30)
             legend.append("Final set")
         if ax:
@@ -203,4 +213,4 @@ if __name__ == "__main__":
             ax.view_init(elev=90, azim=-180)
         plt.show(block=False)
 
-    input("Press enter to exit...")
+        input("Press enter to exit...")
